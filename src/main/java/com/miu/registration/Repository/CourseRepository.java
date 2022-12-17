59cfd7d8b7c04838ac6f297c1376d919ce25ed8c
HIDRI A <agthidri24@yahoo.com>
2022-12-16 19:44:42 -0600

    adding repository student

---
+++ src/main/java/com/miu/registration/Repository/CourseRepository.java
@@ -0,0 +1,7 @@
+package com.miu.registration.Repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CourseRepository {
+}
---
+++ src/main/java/com/miu/registration/controller/CourseController.java
@@ -0,0 +1,4 @@
+package com.miu.registration.controller;
+
+public class CourseController {
+}
