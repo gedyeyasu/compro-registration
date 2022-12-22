f48eff35f23a40201ba1400bcc0023ac456425b6
Gedeon Tona <gedyeyasu@gmail.com>
2022-12-22 14:09:51 -0600

    fixed health check

--- src/main/java/com/miu/registration/security/WebSecurityConfig.java
+++ src/main/java/com/miu/registration/security/WebSecurityConfig.java
@@ -51,7 +51,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         // We don't need CSRF
         httpSecurity.csrf().disable()
                 // dont authenticate this particular request
-                .authorizeRequests().antMatchers("/auth/register", "/auth/login").permitAll().
+                .authorizeRequests().antMatchers("/auth/register", "/auth/login", "/").permitAll().
                 // all other requests need to be authenticated
                         anyRequest().authenticated().and().
                 // make sure we use stateless session; session won't be used to
